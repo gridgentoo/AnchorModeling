0823091eeaf54bc02a843394f2857fd841e8b208
MusicNova <musicnova@yandex.ru>
2018-11-22 17:39:10 +0300

    0 up

--- test/2018/nov/22/iii/ansible/03_airflow_install.sh
+++ test/2018/nov/22/iii/ansible/03_airflow_install.sh
@@ -15,7 +15,7 @@ test "$N" -le "009" && sudo pip install virtualenv
 test "$N" -le "010" && sudo mkdir -p /opt/airflow/airflow_home
 
 test "$N" -le "011" && sudo adduser --quiet --disabled-password --home /opt/airflow/airflow_home airflow || true
-test "$N" -le "012" && sudo chmod -R airflow /opt/airflow/airflow_home
+test "$N" -le "012" && sudo chown -R airflow /opt/airflow/airflow_home
 
 test "$N" -le "013" && (cd /opt/airflow/airflow_home/ && sudo -u airflow virtualenv -v env)
 test "$N" -le "014" && sudo -u airflow sh /opt/airflow/airflow_home/env/bin/activate
@@ -29,5 +29,5 @@ test "$N" -le "020" && sudo sed -i 's#sql_alchem_conn = postgresql+psycopg2://ai
 test "$N" -le "021" && sudo sed -i 's#broker_url = redis://127.0.0.1:6379/1#broker_url = redis://127.0.0.1:6379/1#g' /opt/airflow/airflow_home/web/airflow.cfg.sample
 test "$N" -le "022" && sudo sed -i 's#celery_result_backend = db+postgres://airflow:airflow@localhost:5432/airflow#celery_result_backend = db+postgres://airflow:airflow@localhost:5432/airflow#g' /opt/airflow/airflow_home/web/airflow.cfg.sample
 
-test "$N" -le "023" && sudo chmod -R airflow /opt/airflow/airflow_home/web/airflow.cfg.sample
+test "$N" -le "023" && sudo chown -R airflow /opt/airflow/airflow_home/web/airflow.cfg.sample
 test "$N" -le "024" && sudo -u airflow /opt/airflow/airflow_home/web/airflow/bin/airflow initdb
