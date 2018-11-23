ac8e9d1f633341af27f800967bae4c43ce00fbe9
MusicNova <musicnova@yandex.ru>
2018-11-23 10:03:14 +0300

    0 up

--- test/2018/nov/22/iii/ansible/01_postgres_install.sh
+++ test/2018/nov/22/iii/ansible/01_postgres_install.sh
@@ -5,5 +5,5 @@ set -x -e
 test "$N" -le "001" && sudo apt-get update
 test "$N" -le "002" && sudo apt-get -y install postgresql postgresql-contrib
 test "$N" -le "003" && sudo -u postgres psql -c '\q'
-test "$N" -le "004" && sudo -u postgres createuser --no-superuser sammy --no-password
-test "$N" -le "005" && sudo -u postgres createdb sammy
+test "$N" -le "004" && sudo -u postgres createuser --no-superuser sammy --no-password || true
+test "$N" -le "005" && sudo -u postgres createdb sammy || true
